a50c871f240f297da96e33893d5ef326ebd5a28d
GuessYourGiftle <124407852+GuessYourGiftle@users.noreply.github.com>
2023-02-04 10:52:53 -0500

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -179,7 +179,7 @@ function update() {
 
         if (correct == width) {
             gameOver = true;
-            document.getElementById("answer").innerText = "Who Is Steve Kelly The Lead Singer Of?";
+            document.getElementById("answer").innerText = "Who Is Steve Perry The Lead Singer Of?";
         }
     }
 
